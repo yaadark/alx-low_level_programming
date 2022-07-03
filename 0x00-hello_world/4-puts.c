528d0677fbcc35146e74c15de10f6a40f761a3f2
yaadark <yaalartebeadarko@gmail.com>
2022-07-02 18:03:43 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
-#include <studio.h>
+#include <stdio.h>
 /**
  * *main - Entry point
  * *Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
